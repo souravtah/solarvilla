9b889c0124c340df53066bdc5be3eedd418ce655
souravtah <souravtah@gmail.com>
2024-08-22 23:38:01 +0530

    composer updated as on 22-08-2024

--- database/seeders/RoleAndPermissionSeeder.php
+++ database/seeders/RoleAndPermissionSeeder.php
@@ -3,9 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-use Spatie\Permission\Models\Role;
 use Spatie\Permission\Models\Permission;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Spatie\Permission\Models\Role;
 
 class RoleAndPermissionSeeder extends Seeder
 {
@@ -35,13 +34,13 @@ class RoleAndPermissionSeeder extends Seeder
         Permission::create(['name' => 'create tickets']);
         Permission::create(['name' => 'manage tickets']);
 
-        // create permissions users
+        // create permissions products
         Permission::create(['name' => 'view products']);
         Permission::create(['name' => 'edit products']);
         Permission::create(['name' => 'delete products']);
         Permission::create(['name' => 'create products']);
 
-        // create permissions users
+        // create permissions productCategories
         Permission::create(['name' => 'view productCategories']);
         Permission::create(['name' => 'edit productCategories']);
         Permission::create(['name' => 'delete productCategories']);
@@ -49,15 +48,17 @@ class RoleAndPermissionSeeder extends Seeder
 
         Role::create(['name' => 'SuperAdmin'])->givePermissionTo(Permission::all());
         Role::create(['name' => 'Admin'])->givePermissionTo(Permission::all());
-        Role::create(['name' => 'BackOffice']);
+        Role::create(['name' => 'BackOffice'])->givePermissionTo([
+            'view products',
+            'edit products',
+            'create products',
+            'view productCategories',
+            'edit productCategories',
+            'create productCategories',
+        ]);
         Role::create(['name' => 'TeamLeader']);
         Role::create(['name' => 'Sales']);
         Role::create(['name' => 'Technician']);
-        Role::create(['name' => 'Client'])
-                ->givePermissionTo([
-                    'view tickets list',
-                    'create tickets',
-                    'view each tickets'
-                ]);
+        Role::create(['name' => 'Client']);
     }
 }
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,7 +4,6 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use Illuminate\Database\Seeder;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class UserSeeder extends Seeder
 {
@@ -24,59 +23,59 @@ class UserSeeder extends Seeder
         //     });
         User::unsetEventDispatcher();
         User::create([
-                'name'           => 'SuperAdmin',
-                'email'          => 'superadmin@solarvilla.co.in',
-                'phone'          => '9999999990',
-                'password'       => bcrypt('password@2023'),
-                'remember_token' => null,
+            'name' => 'SuperAdmin',
+            'email' => 'superadmin@solarvilla.co.in',
+            'phone' => '9999999990',
+            'password' => bcrypt('password@2094'),
+            'remember_token' => null,
         ])->assignRole(['SuperAdmin']);
 
         User::create([
-                'name'           => 'Admin',
-                'email'          => 'admin@solarvilla.co.in',
-                'phone'          => '9999999991',
-                'password'       => bcrypt('password@2023'),
-                'remember_token' => null,
+            'name' => 'Admin',
+            'email' => 'admin@solarvilla.co.in',
+            'phone' => '9999999991',
+            'password' => bcrypt('password@2090'),
+            'remember_token' => null,
         ])->assignRole(['Admin']);
 
         User::create([
-                'name'           => 'BackOffice',
-                'email'          => 'backoffice@solarvilla.co.in',
-                'phone'          => '9999999992',
-                'password'       => bcrypt('password@2023'),
-                'remember_token' => null,
+            'name' => 'BackOffice',
+            'email' => 'backoffice@solarvilla.co.in',
+            'phone' => '9999999992',
+            'password' => bcrypt('password@2023'),
+            'remember_token' => null,
         ])->assignRole(['BackOffice']);
 
         User::create([
-                'name'           => 'TeamLeader',
-                'email'          => 'teamleader@solarvilla.co.in',
-                'phone'          => '9999999993',
-                'password'       => bcrypt('password@2023'),
-                'remember_token' => null,
+            'name' => 'TeamLeader',
+            'email' => 'teamleader@solarvilla.co.in',
+            'phone' => '9999999993',
+            'password' => bcrypt('password@2023'),
+            'remember_token' => null,
         ])->assignRole(['TeamLeader']);
 
         User::create([
-                'name'           => 'Sales',
-                'email'          => 'sales@solarvilla.co.in',
-                'phone'          => '9999999994',
-                'password'       => bcrypt('password@2023'),
-                'remember_token' => null,
+            'name' => 'Sales',
+            'email' => 'sales@solarvilla.co.in',
+            'phone' => '9999999994',
+            'password' => bcrypt('password@2023'),
+            'remember_token' => null,
         ])->assignRole(['Sales']);
 
         User::create([
-                'name'           => 'Technician',
-                'email'          => 'technician@solarvilla.co.in',
-                'phone'          => '9999999995',
-                'password'       => bcrypt('password@2023'),
-                'remember_token' => null,
+            'name' => 'Technician',
+            'email' => 'technician@solarvilla.co.in',
+            'phone' => '9999999995',
+            'password' => bcrypt('password@2023'),
+            'remember_token' => null,
         ])->assignRole(['Technician']);
 
         User::create([
-                'name'           => 'Client',
-                'email'          => 'client@solarvilla.co.in',
-                'phone'          => '9999999996',
-                'password'       => bcrypt('password@2023'),
-                'remember_token' => null,
+            'name' => 'Client',
+            'email' => 'client@solarvilla.co.in',
+            'phone' => '9999999996',
+            'password' => bcrypt('password@2023'),
+            'remember_token' => null,
         ])->assignRole(['Client']);
 
     }
